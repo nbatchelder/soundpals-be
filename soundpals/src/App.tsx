8b89747ecd75c4baddc5dd6ccc2a4721e88a33c5
Nickie Pucel <nicholas_pucel@brown.edu>
2018-04-17 23:20:18 -0700

    Add basic TypeScript React app

---
+++ soundpals/src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to SoundPals</h1>
+        </header>
+        <p className="App-intro">
+          View the songs you listen to the most!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
